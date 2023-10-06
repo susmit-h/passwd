1d161db7779fc0585dbb067cff450e199d9b7cb2
Susmit H <131420002+susmit-h@users.noreply.github.com>
2023-10-06 11:55:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+password = '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@!#$%^&*()_+-~?=|\/:;"'<>'.'''
+length = int(input('Enter the length of the password: '))
+
+random_password = random.sample(password,length)
+
+
+print(f"Your password is {final_password}")
